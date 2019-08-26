2859de7ad0b514c47fc7d36186f08e2b62792521
Marcus Thuvesen <Marcus.Thuvesen@cybercom.com>
2019-08-26 10:37:52 +0200

    UI

---
+++ Tinnitus/Views/PopupCollectionView_UI.swift
@@ -0,0 +1,79 @@
+//
+//  PopupCollectionView_UI.swift
+//  Tinnitus
+//
+//  Created by Marcus Thuvesen on 2019-08-24.
+//  Copyright © 2019 Marcus Thuvesen. All rights reserved.
+//
+
+import UIKit
+
+class PopupCollectionView_UI : PremiumPopupVC_UI, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    var sliderImageArray = [UIImage(named: "stop"), UIImage(named: "ecg-lines2"), UIImage(named: "star2"), UIImage(named: "openLock2")]
+    var sliderLabelArray = ["No Ads", "Unlimited Cure", "Save Favorites", "Unlock Sounds"]
+    var sliderSubLabelArray = ["Remove all ads", "Use Tinnitus Matcher unlimited", "Favorite your mixed sounds for later use", "Access all sounds from the library"]
+    var sliderTimer = Timer()
+    var counter = 0
+    var popupCV : UICollectionView!
+    var pageController : UIPageControl!
+    
+    @objc func sliderChangeImage(){
+        if counter < sliderImageArray.count{
+            scrollToCV_Image()
+            counter += 1
+        } else{
+            counter = 0
+            scrollToCV_Image()
+            counter = 1
+        }
+    }
+    
+    func scrollToCV_Image(){
+        let index = IndexPath.init(item: counter, section: 0)
+        popupCV.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+        pageController.currentPage = counter
+    }
+    
+    init(popupCVPageController : UIPageControl, popupCollectionView : UICollectionView){
+        super.init(nibName: "PremiumPopupVC_UI", bundle: nil)
+        self.popupCV = popupCollectionView
+        self.pageController = popupCVPageController
+        popupCVPageController.numberOfPages = sliderImageArray.count
+        popupCVPageController.currentPage = 0
+        popupCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        sliderTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(sliderChangeImage), userInfo: nil, repeats: true)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sliderImageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PopupCV_Cell", for: indexPath) as! PopupSlider_CollectionViewCell
+        if let arrayImage = sliderImageArray[indexPath.row]{
+            cell.sliderCellImage.image = arrayImage
+            cell.sliderCellTitleLabel.text = sliderLabelArray[indexPath.row]
+            cell.sliderCellSubLabel.text = sliderSubLabelArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+}
--- Tinnitus/Views/PremiumPopupVC_UI.swift
+++ Tinnitus/Views/PremiumPopupVC_UI.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PremiumPopupVC_UI: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+class PremiumPopupVC_UI: UIViewController, UIPageViewControllerDelegate{
     
     
     @IBOutlet weak var backgroundPopupImage: UIImageView!
@@ -24,42 +24,29 @@ class PremiumPopupVC_UI: UIViewController, UICollectionViewDataSource, UICollect
     @IBOutlet weak var secondPopupBtnView: UIView!
     @IBOutlet weak var thirdPopupBtnView: UIView!
     
-    @IBOutlet weak var sliderCollectionView: UICollectionView!
-    @IBOutlet weak var sliderPageController: UIPageControl!
+    @IBOutlet weak var popupCollectionView: UICollectionView!
+    @IBOutlet weak var popupCVPageController: UIPageControl!
+   var test = 5
     
-    var sliderImageArray = [UIImage(named: "stop"), UIImage(named: "ecg-lines2"), UIImage(named: "star2"), UIImage(named: "openLock2")]
-    var sliderLabelArray = ["No Ads", "Unlimited Cure", "Save Favorites", "Unlock Sounds"]
-    var sliderSubLabelArray = ["Remove all ads", "Use Tinnitus Matcher unlimited", "Favorite your mixed sounds for later use", "Access all sounds from the library"]
-    var sliderTimer = Timer()
-    var counter = 0
+    lazy var popupCollectionView_UI: PopupCollectionView_UI = {
+        print("in lazy")
+        let popupCollectionView_UI = PopupCollectionView_UI(popupCVPageController : popupCVPageController, popupCollectionView : popupCollectionView)
+        
+        return popupCollectionView_UI
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        sliderPageController.numberOfPages = sliderImageArray.count
-        sliderPageController.currentPage = 0
-        sliderCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-
-        DispatchQueue.main.async {
-            self.sliderTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.sliderChangeImage), userInfo: nil, repeats: true)
-        }
+        popupCollectionView.dataSource = self.popupCollectionView_UI
+        popupCollectionView.delegate = self.popupCollectionView_UI
+        
         setupPremiumPopupUI()
+        
+        
     }
     
-    @objc func sliderChangeImage(){
-        if counter < sliderImageArray.count{
-            let index = IndexPath.init(item: counter, section: 0)
-            self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
-            sliderPageController.currentPage = counter
-            counter += 1
-        } else{
-            counter = 0
-            let index = IndexPath.init(item: counter, section: 0)
-            self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
-            sliderPageController.currentPage = counter
-            counter = 1
-        }
-    }
+    
     
     func setupPremiumPopupUI(){
         premiumContainerView.normalButtonUI()
@@ -69,49 +56,12 @@ class PremiumPopupVC_UI: UIViewController, UICollectionViewDataSource, UICollect
         continueBtnOutlet.premiumPopupContinueUI()
         backgroundPopupImage.addBlurEffect()
         premiumAvatarImage.goldBackground()
-//        firstSubBtnOutlet.popupButtonsUI()
-//        secondSubBtnOutlet.selectedPopupButtonUI()
-//        thirdSubBtnOutlet.popupButtonsUI()
         mostPopularView.mostPopularButtonUI()
         firstPopupBtnView.disselectedPopupButtonsUI()
         secondPopupBtnView.selectedPopupButtonUI()
         thirdPopupBtnView.disselectedPopupButtonsUI()
     }
     
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sliderImageArray.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sliderCell", for: indexPath) as! PopupSlider_CollectionViewCell
-        if let arrayImage = sliderImageArray[indexPath.row]{
-            cell.sliderCellImage.image = arrayImage
-            cell.sliderCellTitleLabel.text = sliderLabelArray[indexPath.row]
-            cell.sliderCellSubLabel.text = sliderSubLabelArray[indexPath.row]
-        }
-        
-        return cell
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = sliderCollectionView.frame.size
-        return CGSize(width: size.width, height: size.height)
-    }
-
     @IBAction func dismissPopupBtn(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
