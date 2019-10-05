f25258652cae35f3fb2328ebdcfc5907b4908eda
Marcus Thuvesen <Marcus.Thuvesen@cybercom.com>
2019-10-05 15:36:37 +0200

    favoriteIMG fix

---
+++ Tinnitus/Presenters/NewFavoritePopupPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  NewFavoritePopupPresenter.swift
+//  Tinnitus
+//
+//  Created by Marcus Thuvesen on 2019-10-02.
+//  Copyright © 2019 Marcus Thuvesen. All rights reserved.
+//
+
+import Foundation
